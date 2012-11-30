abebbdc9d67226ac99899d59849f3fcd2d52d559
cmb69 <cmbecker69@gmx.de>
2012-11-30 02:03:25 +0000

    imported initial version

---
+++ index.php
@@ -0,0 +1,131 @@
+<?php
+
+/**
+ * Front-end of Adventcalendar_XH.
+ *
+ * Copyright (c) 2012 Christoph M. Becker (see license.txt)
+ */
+
+
+if (!defined('CMSIMPLE_XH_VERSION')) {
+    header('HTTP/1.0 403 Forbidden');
+    exit;
+}
+
+
+define('ADVENTCALENDAR_VERSION', '1beta1');
+
+
+/**
+ * Returns the list of indexes of direct children of page no. $n.
+ *
+ * @param  int $n
+ * @param  bool $ignoreHidden  Whether hidden pages should be ignored.
+ * @return array of int.
+ */
+function Adventcalendar_childPages($n, $ignoreHidden = true)
+{
+    global $cl, $l, $cf;
+    
+    $res = array();
+    $ll = $cf['menu']['levelcatch'];
+    for ($i = $n + 1; $i < $cl; $i++) {
+        if ($ignoreHidden && hide($i)) {
+            continue;
+        }
+        if ($l[$i] <= $l[$n]) {
+            break;
+        }
+        if ($l[$i] <= $ll) {
+            $res[] = $i;
+            $ll = $l[$i];
+        }
+    }
+    return $res;
+}
+
+
+/**
+ * Returns the index of the first page with the $heading.
+ *
+ * @param  string $heading
+ * @return int
+ */
+function Adventcalendar_pageIndex($heading)
+{
+    global $cl, $h;
+    
+    for ($i = 0; $i < $cl; $i++) {
+        if ($h[$i] == $heading) {
+            return $i;
+        }
+    }
+    return null;
+}
+
+
+/**
+ * Returns the path of the data folder.
+ *
+ * @return string
+ */
+function Adventcalendar_dataFolder()
+{
+    global $pth;
+    
+    return "{$pth['folder']['plugins']}adventcalendar/data/";
+}
+
+
+/**
+ * Emits the required scripts to the <head>.
+ *
+ * @return void
+ */
+function Adventcalendar_js()
+{
+    global $pth, $plugin_cf;
+    
+    $href = "{$pth['folder']['plugins']}adventcalendar/css/colorbox.css";
+    include "{$pth['folder']['plugins']}jquery/jquery.inc.php";
+    include_jQuery();
+    $js = "{$pth['folder']['plugins']}adventcalendar/colorbox/jquery.colorbox-min.js";
+    include_jQueryPlugin('colorbox', $js);
+}
+
+
+/**
+ * Returns the advent calendar view.
+ *
+ * @access public
+ * @param  string $cal  The name of the calendar.
+ * @return string  The (X)HTML.
+ */
+function Adventcalendar($cal)
+{
+    global $pth, $u, $plugin_cf;
+    
+    $pcf = $plugin_cf['adventcalendar'];
+    $day = intval(floor((time() - $pcf['time_start']) / 86400)) + 1;
+    //$day = 24; // for testing
+    $data = unserialize(file_get_contents(Adventcalendar_dataFolder() . $cal . '.dat'));
+    $src = Adventcalendar_dataFolder() . $cal . '+.jpg';
+    $n = Adventcalendar_pageIndex($cal);
+    $pages = Adventcalendar_childPages($n);
+    Adventcalendar_js();
+    $o = tag('img src="' . $src . '" usemap="#adventcalendar"')
+        . '<map name="adventcalendar">';
+    for ($i = 0; $i < $day; $i++) {
+        if (array_key_exists($i, $pages)) {
+            $coords = $data[$i];
+            $href = $u[$pages[$i]] . '&amp;print';
+            $o .= tag('area shape="rect" coords="' . implode(',', $coords) . '" href="?' . $href
+                      . '" onclick="jQuery.colorbox({maxWidth:\'80%\',href:\'?' . $href . '\'}); return false"');
+        }
+    }
+    $o .= '</map>';
+        
+    return $o;
+}
+
+?>
---
+++ languages/de.php
@@ -0,0 +1,20 @@
+<?php
+
+$plugin_tx['adventcalendar']['prepare_cover']="Umschlag vorbereiten";
+
+$plugin_tx['adventcalendar']['syscheck_title']="System-Prüfung";
+$plugin_tx['adventcalendar']['syscheck_phpversion']="PHP-Version ≥ %s";
+$plugin_tx['adventcalendar']['syscheck_extension']="Erweiterung '%s' geladen";
+$plugin_tx['adventcalendar']['syscheck_encoding']="UTF-8-Kodierung eingestellt";
+$plugin_tx['adventcalendar']['syscheck_magic_quotes']="Magic quotes runtime off";
+$plugin_tx['adventcalendar']['syscheck_jquery']="<a href=\"http://www.cmsimple-xh.com/wiki/doku.php/plugins:jquery4cmsimple\">jQuery4CMSimple-Plugin</a> installiert";
+$plugin_tx['adventcalendar']['syscheck_writable']="Ordner '%s' beschreibbar";
+
+$plugin_tx['adventcalendar']['cf_door_width']="Die Breite der Türchen in Pixeln.";
+$plugin_tx['adventcalendar']['cf_door_height']="Die Höhe der Türchen in Pixeln.";
+$plugin_tx['adventcalendar']['cf_color_door']="Die Farbe des Rahmens der Türchen als hexadezimaler RGB-Wert (z.B. ist \"000000\" schwarz und \"ffffff\" ist weiß).";
+$plugin_tx['adventcalendar']['cf_color_font']="Die Farbe der Tages-Nummern innerhalb der Türchen als hexadezimaler RGB-Wert (z.B. ist \"000000\" schwarz und \"ffffff\" ist weiß).";
+$plugin_tx['adventcalendar']['cf_color_fringe']="Die Farbe der Umrandung der Tages-Nummern innerhalb der Türchen als hexadezimaler RGB-Wert (z.B. ist \"000000\" schwarz und \"ffffff\" ist weiß).";
+$plugin_tx['adventcalendar']['cf_time_start']="Der Unix-Zeitstempel des Start-Tags des Adventskalenders. Die Voreinstellung von \"1354320000\" ist der Zeitstempel für 2012 (GMT). Verwenden Sie einen Umrechner wie http://www.4webhelp.net/us/timestamp.php um diesen an Ihre Zeitzone und das Jahr anzupassen.";
+
+?>
