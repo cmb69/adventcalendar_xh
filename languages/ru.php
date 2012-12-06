7d9b2784f63ab05588e6bee15dba9ba3a5ce96cc
cmb69 <cmbecker69@gmx.de>
2012-12-06 20:31:13 +0000

    added Russian translation and user manual (contributed by Old)

---
+++ languages/ru.php
@@ -0,0 +1,34 @@
+<?php
+
+/*
+ * Copyright (c) 2012 Lybomyr Kydray Old aka
+ *
+ * Translation are licensed under the GNU General Public License, version 2 or
+ * later.
+ */
+
+/* added */
+$plugin_tx['adventcalendar']['menu_main']="Управление";
+$plugin_tx['adventcalendar']['menu_css']="Стили";
+$plugin_tx['adventcalendar']['menu_config']="Конфигурация";
+$plugin_tx['adventcalendar']['menu_language']="Язык";
+$plugin_tx['adventcalendar']['menu_help']="Помощь";
+//
+$plugin_tx['adventcalendar']['prepare_cover']="Подготовка обложки";
+
+$plugin_tx['adventcalendar']['syscheck_title']="Проверка системы";
+$plugin_tx['adventcalendar']['syscheck_phpversion']="Версия PHP &ge; %s";
+$plugin_tx['adventcalendar']['syscheck_extension']="Расширение '%s' загружено";
+$plugin_tx['adventcalendar']['syscheck_encoding']="Кодировка 'UTF-8' установлена";
+$plugin_tx['adventcalendar']['syscheck_magic_quotes']="Экранирование спецсимволов (\"magic_quotes_runtime\") отключено";
+$plugin_tx['adventcalendar']['syscheck_jquery']="Плагин <a href=\"http://www.cmsimple-xh.com/wiki/doku.php/plugins:jquery4cmsimple\">jQuery4CMSimple</a> установлен";
+$plugin_tx['adventcalendar']['syscheck_writable']="Папка '%s' доступна для записи";
+
+$plugin_tx['adventcalendar']['cf_door_width']="Ширина небольших дверей в пикселях.";
+$plugin_tx['adventcalendar']['cf_door_height']="Высота небольших дверей в пикселях.";
+$plugin_tx['adventcalendar']['cf_color_door']="Цвет рамки небольших дверей - шестнадцатеричное значение RGB (например, \"000000 \" - черный и \"ffffff \" - белый).";
+$plugin_tx['adventcalendar']['cf_color_font']="Цвет дат внутри небольших дверей - шестнадцатеричное значение RGB (например, \"000000 \" - черный и \"ffffff \" - белый).";
+$plugin_tx['adventcalendar']['cf_color_fringe']="Цвет каймы дат внутри небольших дверей - шестнадцатеричное значение RGB (например, \"000000 \" - черный и \"ffffff \" - белый).";
+$plugin_tx['adventcalendar']['cf_time_start']="Дата начала календаря рождественского поста (в формате метки времени Unix). Значение по умолчанию: \"1354320000 \" - является меткой времени для 2012 года (по Гринвичу). Используйте конвертер, например http://www.4webhelp.net/us/timestamp.php, чтобы указать дату в соответствии с Вашей временной зоной и годом.";
+
+?>
