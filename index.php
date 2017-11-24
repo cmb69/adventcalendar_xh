a20d709d5c7c40775ae89c72783fdb579384a7f7
Christoph M. Becker <cmbecker69@gmx.de>
2017-11-24 14:17:28 +0100

    No need for this guard
    If index.php will be requested directly, it will fail anyway without
    doing any harm.  If the plugin is used under unsupported CMSimple_XH
    versions, it is a user error.

--- index.php
+++ index.php
@@ -19,22 +19,6 @@
  * along with Adventcalendar_XH.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-/*
- * Prevent direct access and usage from unsupported CMSimple_XH versions.
- */
-if (!defined('CMSIMPLE_XH_VERSION')
-    || strpos(CMSIMPLE_XH_VERSION, 'CMSimple_XH') !== 0
-    || version_compare(CMSIMPLE_XH_VERSION, 'CMSimple_XH 1.6', 'lt')
-) {
-    header('HTTP/1.1 403 Forbidden');
-    header('Content-Type: text/plain; charset=UTF-8');
-    die(<<<EOT
-Adventcalendar_XH detected an unsupported CMSimple_XH version.
-Uninstall Adventcalendar_XH or upgrade to a supported CMSimple_XH version!
-EOT
-    );
-}
-
 /**
  * @param string $cal
  * @return string
