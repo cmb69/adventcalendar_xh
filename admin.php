d97d36a253612efdd6bf86d4079f4acdecc6242b
Christoph M. Becker <cmbecker69@gmx.de>
2023-03-16 16:14:09 +0100

    Fix regression due to typo

--- admin.php
+++ admin.php
@@ -22,7 +22,7 @@
 use Adventcalendar\Dic;
 use Adventcalendar\Infra\Responder;
 
-if (!defined("CMSIMPLE_XH_VERION")) {
+if (!defined("CMSIMPLE_XH_VERSION")) {
     header("HTTP/1.1 403 Forbidden");
     exit;
 }
